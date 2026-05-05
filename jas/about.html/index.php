8b3e6d674967d76151080e1b02644e0b30ff985a
jasralainetimbol295-ctrl <jasralainetimbol295@gmail.com>
2026-05-05 11:01:05 +0800

    Add files via upload

---
+++ jas/about.html/index.php
@@ -0,0 +1,19 @@
+!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php include 'includes/header.php'; ?>
+
+<section class="hero">
+  <h2>Welcome to My Portfolio</h2>
+  <p>I am a Web Design student passionate about building modern websites.</p>
+</section>
+
+<?php include 'includes/footer.php'; ?>
+
+</body>
+</html>
